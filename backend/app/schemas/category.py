749e5ee7fb523833680a8e946ecb69cf7bc5bc32
ban.der4 <ctepanskinder@yandex.ru>
2025-11-18 16:19:33 +0300

    first commit, add structure and schemas category

---
+++ backend/app/schemas/category.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+from enum import Enum
+
+class CategoryType(str,Enum):
+    INCOME = "income"
+    EXPENSE = "expense"
+    TRANSFER = "transfer"
+
+class CategoryBase(BaseModel):
+    name: str = Field(..., description = "Category name" , min_length=3, max_length=200)
+    description : str = Field(None, description = "Category description" , min_length=3, max_length=200)
+    icon_url: str = Field(None, description = "Category icon url")
+    type: CategoryType = Field(..., description = "Category type")
+    parent_id: Optional[int] = Field(None, description="Parent category ID")
+
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class CategoryResponse(CategoryBase):
+    id : int = Field(...,description='Unique category identifier')
+    class Config:
+        from_attributes = True
