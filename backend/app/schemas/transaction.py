dc7171ec413512f07a4b3db65fd63fbab667d752
ban.der4 <ctepanskinder@yandex.ru>
2025-11-19 16:48:22 +0300

    add transaction.py

---
+++ backend/app/schemas/transaction.py
@@ -0,0 +1,26 @@
+from datetime import date, datetime
+from enum import Enum
+
+from pydantic import BaseModel, Field
+from typing import Optional
+class TransactionType(str, Enum):
+    income = "income"
+    expense = "expense"
+    transfer = "transfer"
+class TransactionBase(BaseModel):
+    amount: float = Field(..., description="Transaction amount", gt=0)
+    name : str = Field(..., description="Transaction name")
+    description : Optional[str] = Field(None, description="Transaction description",min_length=3,max_length=300)
+    date : Optional[date] = Field(None, description="Transaction date")
+    type : TransactionType = Field(..., description="Transaction type")
+
+class TransactionCreate(TransactionBase):
+    pass
+
+class TransactionResponse(TransactionBase):
+    id: int = Field(..., description="Transaction ID")
+    created_at: datetime = Field(..., description="Transaction created at")
+    class Config:
+        from_attributes = True
+
+
