be5edf099b6ccd3efb678c99d1721acd4494e038
ban.der4 <ctepanskinder@yandex.ru>
2025-11-22 13:34:29 +0300

    add category_repository

---
+++ backend/app/repositories/category_repository.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Session
+from typing import List,Optional
+from ..schemas.category import CategoryCreate
+from ..models.category import Category
+
+class CategoryRepository():
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_by_id(self, id: int) -> Optional[Category]:
+        return self.db.query(Category).filter(Category.id == id).first()
+
+    def get_by_name(self, name: str) -> Optional[Category]:
+        return self.db.query(Category).filter(Category.name == name).first()
+
+    def get_all(self) -> List[Category]:
+        return self.db.query(Category).all()
+
+    def create(self, data: CategoryCreate) -> Category:
+        category_db = Category(**data.model_dump())
+        self.db.add(category_db)
+        self.db.commit()
+        self.db.refresh(category_db)
+        return category_db
