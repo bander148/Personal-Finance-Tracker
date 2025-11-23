b48ccf060349e34bf400b1cf04c06a53ed473637
ban.der4 <ctepanskinder@yandex.ru>
2025-11-23 11:18:08 +0300

    add routes/categories.py

---
+++ backend/app/routes/categories.py
@@ -0,0 +1,26 @@
+from ..schemas.category import CategoryResponse, CategoryCreate
+from ...database import get_db
+from ..services.category_service import CategoryService
+from fastapi import APIRouter,Depends,status
+from sqlalchemy.orm import Session
+
+
+router = APIRouter(
+    prefix="api/categories",
+    tags=["categories"]
+)
+
+@router.get("")
+async def get_categories(db: Session = Depends(get_db)):
+    service = CategoryService(db)
+    return service.get_all_categories()
+
+@router.get("/{category_id}")
+async def get_category(category_id: int, db: Session = Depends(get_db)):
+    service = CategoryService(db)
+    return service.get_category_by_id(category_id)
+
+@router.post("/create")
+async def create_category(data : CategoryCreate, db: Session = Depends(get_db)):
+    service = CategoryService(db)
+    return service.create_category(data)
