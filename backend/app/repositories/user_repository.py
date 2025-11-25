5293cee3930eb23a6da316063250325cae123863
ban.der4 <ctepanskinder@yandex.ru>
2025-11-25 12:37:01 +0300

    add user_repository

---
+++ backend/app/repositories/user_repository.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from typing import Optional
+from sqlalchemy.orm import Session
+from ..schemas.user import UserCreate
+from passlib.context import CryptContext
+from ..models.user import User
+pwd_context = CryptContext(schemes = ['bcrypt'], deprecated = 'auto')
+
+class UserRepository:
+    def __init__(self, db:Session):
+        self.db = db
+
+    def get_by_email(self, email : str) -> Optional[User]:
+        return self.db.query(User).filter(User.email == email).first()
+
+    def get_by_id(self, id : int) -> Optional[User]:
+        return self.db.query(User).filter(User.id == id).first()
+
+    def create_user(self, user_data: UserCreate) -> User:
+        hashed_password = pwd_context.hash(user_data.password)
+        db_user = User(
+            email = user_data.email,
+            hashed_password= hashed_password,
+            name = user_data.name,
+            created_at = datetime.utcnow(),
+        )
+        self.db.add(db_user)
+        self.db.commit()
+        self.db.refresh(db_user)
+        return db_user
+
+    def authenticate(self, email : str, password : str) -> Optional[User]:
+        user = self.get_by_email(email)
+        if not user or not pwd_context.verify(password, user.hashed_password):
+            return None
+        return user
